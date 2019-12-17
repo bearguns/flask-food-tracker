a09d74dc729bd0eb19b894f42a9adb8975495462
Sean Brage <sean@pop-os.localdomain>
2019-12-16 20:29:28 -0700

    adds DB query for food

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
-from flask import Flask, render_template, g
+import sqlite3
+from flask import Flask, render_template, g, request, redirect, url_for
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_PATH = os.environ('DB_PATH')
+DATABASE_PATH = os.environ.get('DB_PATH')
 
 app = Flask(__name__)
 
@@ -23,7 +24,7 @@ def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
         
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('home.html')
 
@@ -31,9 +32,26 @@ def index():
 def day():
     return render_template('day.html')
 
-@app.route('/add_food')
+@app.route('/food', methods=['GET', 'POST'])
 def food():
-    return render_template('food.html')
+    db = get_db()
+    
+    if request.method == 'POST':
+        name = request.form['name']
+        protein = int(request.form['protein'])
+        carbohydrates = int(request.form['carbohydrates'])
+        fat = int(request.form['fat'])
+        calories = protein * 4 + carbohydrates * 4 + fat * 9
+        
+        db.execute('insert into food (name, protein, carbohydrates, fat, calories) values (?, ?, ?, ?, ?)', [name, protein, carbohydrates, fat, calories])
+        db.commit()
+
+        return redirect(url_for('food'))
+
+    cur = db.execute('select name, protein, carbohydrates, fat, calories from food')
+    results = cur.fetchall()
+    
+    return render_template('food.html', results=results)
 
 if __name__ == '__main__':
     app.run(debug=True)
